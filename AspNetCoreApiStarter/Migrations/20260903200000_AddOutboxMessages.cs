60de60e3b650f8f29cb7e1e788b17b0979018f66
sartim <sarrtim@gmail.com>
2026-09-22 19:16:05 +0300

    fix: associate hand-written migrations with context

--- AspNetCoreApiStarter/Migrations/20260903200000_AddOutboxMessages.cs
+++ AspNetCoreApiStarter/Migrations/20260903200000_AddOutboxMessages.cs
@@ -1,9 +1,12 @@
+using AspNetCoreApiStarter.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace AspNetCoreApiStarter.Migrations;
 
+[DbContext(typeof(ApplicationDbContext))]
 [Migration("20260903200000_AddOutboxMessages")]
 public partial class AddOutboxMessages : Migration
 {
